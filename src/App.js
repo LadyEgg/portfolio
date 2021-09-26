3e554ee0a636edfefad1ecc82ac1a72c82dd198c
Michelle Otero <michelle.otero@lc.cuny.edu>
2021-09-26 00:35:44 -0400

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/LadyEgg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me out!
         </a>
       </header>
     </div>
